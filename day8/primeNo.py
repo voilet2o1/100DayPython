f0218dea7e21e72e4c177b8e033e15806b27341c
voilet2o1 <127921000+voilet2o1@users.noreply.github.com>
2023-06-08 11:52:15 +0530

    Add files via upload

---
+++ day8/primeNo.py
@@ -0,0 +1,17 @@
+def prime_checker(number):
+    is_prime = True
+    for i in range(2, number-1):
+        if number % i == 0:
+            is_prime = False
+    if is_prime:
+        print("It is a prime number.")
+    else:
+        print("It is not a prime number.")
+
+n = int( input("Enter the Number: "))
+prime_checker(number=n)
+
+
+
+
+
